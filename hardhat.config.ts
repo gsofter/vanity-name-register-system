f878aa505067f1c8c4e0b1b8f45f08020cbe7395
gsofter <real.kingstar@outlook.com>
2021-12-07 09:31:45 -0600

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,11 +23,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
   defaultNetwork: "rinkeby",
   networks: {
     rinkeby: {
-      url: process.env.ROPSTEN_URL || "",
+      url: process.env.RINKEBY_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
